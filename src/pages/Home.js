094d95b3d4bd8850a1ce4e1c1cdecdf1b019fd25
Fasad <salatiksama@gmail.com>
2024-07-19 15:37:54 +0300

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import '../styles/home.css'
 import avatar from '../components/wlcpage/headavatar.png'
-
+import { Link } from 'react-router-dom';
+import tg from '../imgs/tg.svg';
+import logofot from '../imgs/logofot.svg';
+import fotlogo from '../imgs/fotlogo.svg'
 function Home() {
     return (
         <div className="container">
@@ -31,6 +34,91 @@ function Home() {
                 <h1>Tasks</h1>
                 <p>Some text Some text Some text Some text Some text Some text</p>
             </div>
+
+
+            <div className='maincontent'>
+                <div className='switches'>
+                    <button className='btn1'>Tasks</button>
+                    <Link to='/'><button className='btn2'>Leaders</button></Link>
+                </div>
+                <div className='miniswitch'>
+                    <button className='minibtnactive'>All</button>
+                    <button>Social acttivity</button>
+                    <button>Manual verification</button>
+                </div>
+                <div className='switchcontent'>
+                    <div className='tasking'>
+                        <img src={tg}></img>
+                        <span className='tskk'><p className='typetask'>Manual verification</p><p className='tsk'>Subscribe on telegram</p></span>
+                        <span>
+                            <div className='valuetask'><p>50 coins</p><button className='claimbtn'>claim</button></div>
+                            
+                        </span>
+                    </div>
+                    <div className='tasking'>
+                        <img src={tg}></img>
+                        <span className='tskk'><p className='typetask'>Manual verification</p><p className='tsk'>Subscribe on telegram</p></span>
+                        <span className='tasksk'>
+                            <div className='valuetask'><p>50 coins</p><button className='claimbtn'>claim</button></div>
+                        </span>
+                    </div>
+                    <div className='tasking'>
+                        <img src={tg}></img>
+                        <span className='tskk'><p className='typetask'>Manual verification</p><p className='tsk'>Subscribe on telegram</p></span>
+                        <span>
+                            <div className='valuetask'><p>50 coins</p><button className='claimbtn'>claim</button></div>
+                            
+                        </span>
+                    </div>
+                    <div className='tasking'>
+                        <img src={tg}></img>
+                        <span className='tskk'><p className='typetask'>Manual verification</p><p className='tsk'>Subscribe on telegram</p></span>
+                        <span>
+                            <div className='valuetask'><p>50 coins</p><button className='claimbtn'>claim</button></div>
+                            
+                        </span>
+                    </div>
+                    <div className='tasking'>
+                        <img src={tg}></img>
+                        <span className='tskk'><p className='typetask'>Manual verification</p><p className='tsk'>Subscribe on telegram</p></span>
+                        <span>
+                            <div className='valuetask'><p>50 coins</p><button className='claimbtn'>claim</button></div>
+                            
+                        </span>
+                    </div>
+                    <div className='tasking'>
+                        <img src={tg}></img>
+                        <span className='tskk'><p className='typetask'>Manual verification</p><p className='tsk'>Subscribe on telegram</p></span>
+                        <span>
+                            <div className='valuetask'><p>50 coins</p><button className='claimbtn'>claim</button></div>
+                            
+                        </span>
+                    </div>
+                    <div className='tasking'>
+                        <img src={tg}></img>
+                        <span className='tskk'><p className='typetask'>Manual verification</p><p className='tsk'>Subscribe on telegram</p></span>
+                        <span>
+                            <div className='valuetask'><p>50 coins</p><button className='claimbtn'>claim</button></div>
+                            
+                        </span>
+                    </div>
+                </div>
+    
+            </div>
+
+            <div className='fot'>
+                <div><img src={logofot}/></div>
+                <div className='fotcont'>
+                    
+                    <div>
+                        <Link to='/'><button><img src={fotlogo}></img></button></Link>
+                        <Link to=''><button><img src={fotlogo}></img></button></Link>
+                        <Link to=''><button><img src={fotlogo}></img></button></Link>
+                    </div>
+                </div>
+                
+            </div>
+
 
         </div>
     );
