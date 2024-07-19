56a024afc0719b08e6d499f0b94308d7c180b1a1
Fasad <salatiksama@gmail.com>
2024-07-19 03:22:13 +0300

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,39 @@
-// Home.js
 import React from 'react';
+import '../styles/home.css'
+import avatar from '../components/wlcpage/headavatar.png'
 
 function Home() {
-  return (
-    <div>
-      <h2>Home Page</h2>
-      <p>Welcome to the home page!</p>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className='headerr'>
+                <div className='nae'>
+                    <span className='nameava'>
+                        <span className='imgheader'>
+                            <img src={avatar} alt='' />
+                            <p>OLEG</p>
+                        </span>
+                        <span className='frenhead'>
+                            <p>5 friends</p>
+                        </span>
+                    </span>
+                    <span className='headbtns'>
+                        <button>
+                            <p>Connect wallet</p>
+                        </button>
+                        <div>
+                            <p>1 000 000 coins</p>
+                        </div>
+                    </span>
+                </div>
+            </div>
+
+            <div className='tasks'>
+                <h1>Tasks</h1>
+                <p>Some text Some text Some text Some text Some text Some text</p>
+            </div>
+
+        </div>
+    );
 }
 
 export default Home;
