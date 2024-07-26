8599e4bb9c2e5e427566f96daf7e4c4455e01f63
Fasad <salatiksama@gmail.com>
2024-07-26 15:03:01 +0300

    yes

--- src/components/wlcpage/wlc1.js
+++ src/components/wlcpage/wlc1.js
@@ -1,22 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSwipeable } from 'react-swipeable';
 import { Link, useNavigate } from 'react-router-dom';
+import QRCode from 'qrcode.react';
 import './wlc.css';
 import Header from '../Header';
 import arrow from './arrow.svg';
 import wlc1 from './wlc1.png';
 import wlc2 from './wlc2.png';
-import wlc3 from './wlc3.png'; // Import all your images similarly
+import wlc3 from './wlc3.png';
 import wlc4 from './wlc4.png';
 import wlc5 from './wlc5.png';
 import wlc6 from './wlc6.png';
 import wlc7 from './wlc7.png';
 
-const images = [wlc1, wlc2, wlc3, wlc4, wlc5, wlc6, wlc7]; // Add all your images here
+const images = [wlc1, wlc2, wlc3, wlc4, wlc5, wlc6, wlc7];
 
 function Wlc1() {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const navigate = useNavigate(); // Hook to access navigation
+  const [isMobile, setIsMobile] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/android/i.test(userAgent) || (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream)) {
+      setIsMobile(true);
+    }
+  }, []);
 
   const handlers = useSwipeable({
     onSwipedLeft: () => handleSwipeLeft(),
@@ -25,7 +34,7 @@ function Wlc1() {
 
   const handleSwipeLeft = () => {
     if (currentIndex === images.length - 1) {
-      navigate('/stylesy'); // Redirect to main page
+      navigate('/stylesy');
     } else {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
     }
@@ -39,28 +48,37 @@ function Wlc1() {
     <div {...handlers}>
       <Header />
       <div className='container'>
-        <img src={images[currentIndex]} alt={`wlc${currentIndex + 1}`} />
+        {isMobile ? (
+          <img src={images[currentIndex]} alt={`wlc${currentIndex + 1}`} />
+        ) : (
+          <div className='qr-container'>
+            <h2>Scan the QR code to open the mini app on your phone</h2>
+            <QRCode value="https://t.me/FasadFinder_bot/JettoCoin" size={256} />
+          </div>
+        )}
       </div>
-      <div className='fot'>
-        <div className='svgcom'>
-          <svg width="156" height="12" viewBox="0 0 156 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-            {images.map((_, index) => (
-              <circle key={index} cx={6 + 24 * index} cy="6" r="6" fill={currentIndex === index ? "#3481DC" : "white"} />
-            ))}
-          </svg>
-        </div>
-        <div className='footerr'>
-          <div className='btn-group'>
-            <Link to="/stylesy">
-              <button className='skip-btn'><p>Skip</p></button>
-            </Link>
-            <div className='margin'></div>
-            <button className='next-btn' onClick={handleSwipeLeft}>
-              <p>Next</p><img src={arrow} alt='' />
-            </button>
+      {isMobile && (
+        <div className='fot'>
+          <div className='svgcom'>
+            <svg width="156" height="12" viewBox="0 0 156 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+              {images.map((_, index) => (
+                <circle key={index} cx={6 + 24 * index} cy="6" r="6" fill={currentIndex === index ? "#3481DC" : "white"} />
+              ))}
+            </svg>
+          </div>
+          <div className='footerr'>
+            <div className='btn-group'>
+              <Link to="/stylesy">
+                <button className='skip-btn'><p>Skip</p></button>
+              </Link>
+              <div className='margin'></div>
+              <button className='next-btn' onClick={handleSwipeLeft}>
+                <p>Next</p><img src={arrow} alt='' />
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
